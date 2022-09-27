bfebeece832d45290d7e5a48bf3a645691954ea0
YiniXu9506 <xuyini@pingcap.com>
2022-09-27 15:19:29 +0800

    fix: chart style

--- src/utils/charts.ts
+++ src/utils/charts.ts
@@ -14,6 +14,7 @@ import dayjs from 'dayjs'
 import React, { useRef } from 'react'
 import { DEFAULT_MIN_INTERVAL_SEC } from './prometheus'
 import '@elastic/charts/dist/theme_only_light.css'
+import './chart.css'
 import tz from './timezone'
 
 /**
