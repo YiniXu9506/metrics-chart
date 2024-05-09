fb64497f7db3b6dc4d016b6d14878b5dc8db0dd9
Suhaha <jklopsdfw@gmail.com>
2024-05-09 13:32:24 +0800

    chore: update deps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import MetricsChart from './MetricChart'
-import './.css/light_theme.css'
-import './.css/dark_theme.css'
 
 export * from './MetricChart/interfaces'
 export * from './MetricChart/SyncChartPointerContext'
